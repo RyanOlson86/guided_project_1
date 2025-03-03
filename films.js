c8999b6c763308b6cf2877f84008c47ed370f86f
Florent Elom <komlavi.elom@outlook.com>
2025-03-03 17:47:39 -0500

    Planet layer added

--- films.js
+++ films.js
@@ -21,19 +21,20 @@ addEventListener('DOMContentLoaded', () => {
 async function getFilms(id) {
     let films;
     let characters;
-    //let 
+    let planets;
+
     try {
         films = await fetchFilms(id)
         characters = await fetchCharacter(id)
-        //   character.homeworld = await fetchHomeworld(character)
-        //   character.films = await fetchFilms(character)
+        planets = await fetchPlanets(id)
         console.log(films);
         console.log(characters);
+        console.log(planets);
     }
     catch (ex) {
         console.error(`Error reading character ${id} data.`, ex.message);
     }
-    renderFilms(films,characters);
+    renderFilms(films, characters, planets);
 
 }
 async function fetchHomeworld(character) {
@@ -49,7 +50,7 @@ async function fetchCharacter(id) {
         .then(res => res.json())
 
 }
-// get films data
+
 async function fetchFilms(id) {
     const url = `${baseUrl}/films/${id}`;
     const films = await fetch(url)
@@ -57,18 +58,30 @@ async function fetchFilms(id) {
     return films;
 }
 
-const renderFilms = (films, characters) => {
+async function fetchPlanets(id) {
+    const url = `${baseUrl}/films/${id}/planets`;
+    const planets = await fetch(url)
+        .then(res => res.json())
+    return planets;
+}
+
+const renderFilms = (films, characters, planets) => {
     document.title = `SWAPI - ${films?.title}`;  // Just to make the browser tab say their name
     nameH1.textContent = films?.title;
     releaseno.textContent = films?.release_date;
     directorname.textContent = films?.director;
     episodeno.textContent = films?.episode_id;
-    //    // homeworldSpan.innerHTML = `<a href="/planet.html?id=${films?.homeworld.id}">${films?.homeworld.name}</a>`;
-       // const filmsLis = films?.films?.map(film => `<li><a href="/film.html?id=${film.id}">${film.title}</li>`)
-        const charactersList = characters.map((char)=> {
-          return `<li><a href="/character.html?id=${char.id}">${char.name}</a></li>`
-        })
-        document.querySelector('#characterList').innerHTML = charactersList;
 
-    //     filmsUl.innerHTML = filmsLis.join("");
+    const charactersList = characters.map((char) => {
+        return `<li><a href="/character.html?id=${char.id}">${char.name}</a></li>`
+    })
+    document.querySelector('#characterList').innerHTML = charactersList;
+
+    const planetList = planets.map((planet) => {
+        return `<li><a href="/planet.html?id=${planet.id}">${planet.name}</a></li>`
+    })
+
+    document.querySelector('#planetList').innerHTML = planetList;
+
+
 }
